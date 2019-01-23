b7915df9f15d9b3d9ca7bdccfabed93a466043ef
dorothyu <dorothyuyou@hotmail.com>
2019-01-23 16:59:17 +0900

    Create conses.py

---
+++ conses.py
@@ -0,0 +1,43 @@
+import nmf
+import numpy as np
+
+
+def consensus(a, Matrix_RANK, Matrix_ORDER,nloop):
+    """ calculate consensus matrix
+    """
+    (n,m)     = a.shape
+    consensus = np.zeros((m,m))
+    conn      = np.zeros((m,m))
+
+    #i = 0
+    connac = np.zeros((m,m))
+
+    for l in range(nloop):
+            #i += 1
+        (w,h) = nmf.basicnmf(a, Matrix_RANK, Matrix_ORDER, 0.001, 50, 100)
+        conn   = nmfconnectivity(h)
+            
+        connac = connac + conn
+
+    consensus = connac / float(nloop)
+    return consensus
+
+def nmfconnectivity(h):
+    """ calculate connective matrix
+    """
+    (k,m) = h.shape
+
+    ar = []
+    for i in range(m):
+        max_i = 0
+        max_v = 0
+        for index,v in enumerate(h[:,i]):
+            if v > max_v :
+                max_v = v
+                max_i = index
+        ar.append(max_i)
+
+    mat1 = np.tile(np.matrix(ar),(m,1))
+    mat2 = np.tile(np.matrix(ar).T,(1,m))
+
+    return np.array(mat1 == mat2, dtype=int)
