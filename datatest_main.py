fb24a17650d38d4dc15a5b776cfaf8331306fcd5
dorothyu <dorothyuyou@hotmail.com>
2019-01-23 17:00:03 +0900

    Update datatest_main.py

--- datatest_main.py
+++ datatest_main.py
@@ -2,31 +2,44 @@
 
 from numpy import dot
 from numpy import random
-from nmf import nmf
+from nmf import basicnmf
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from conses import consensus
 
-# Define constants
-Matrix_ORDER = 5
-Matrix_RANK = 2
 
-# Generate a target Matrix
-origin_W = random.randint(0,9,size=(Matrix_RANK, Matrix_ORDER))
-origin_H = random.randint(0,9,size=(Matrix_ORDER, Matrix_RANK))
-v = dot(origin_W, origin_H) 
+def main():
 
-# Generate initial Matrix W and H (Random generation)
-w = random.randint(0,9,size=(Matrix_RANK, Matrix_ORDER))
-h = random.randint(0,9,size=(Matrix_ORDER, Matrix_RANK))
+    # Define constants
+    Matrix_ORDER = 5
+    Matrix_RANK = 5
 
-# Calculate W&H by multiple iteration
-(output_W, output_H) = nmf(v, w, h, 0.001, 50, 100)
-
-# Show Results
-print ("\n==Original_Matrix==")
-print ("Matrix_W :\n", origin_W) 
-print ("Matrix_H :\n", origin_H)
-print ("Target_V :\n", v)
-
-print ("\n===Output_Matrix===")
-print ("Matrix_W :\n", output_W)
-print ("Matrix_H :\n", output_H)
-print ("Output_V :\n", dot(output_W, output_H))
+    # Generate a target Matrix
+    origin_W = random.randint(0,9,size=(Matrix_RANK, Matrix_ORDER))
+    origin_H = random.randint(0,9,size=(Matrix_ORDER, Matrix_RANK))
+    v = dot(origin_W, origin_H) 
+    
+    # Calculate W&H by multiple iteration
+    # def nmf(V,Winit,Hinit,tol,timelimit,maxiter)
+    (output_W, output_H) = basicnmf(v, Matrix_RANK, Matrix_ORDER, 0.001, 50, 100)
+    cons = consensus(v, Matrix_RANK, Matrix_ORDER,5)
+    print("Consensus:\n",cons)
+    sns.heatmap(cons)
+    
+    # Show Results
+    print ("\n==Original_Matrix==")
+    print ("Matrix_W :\n", origin_W) 
+    print ("Matrix_H :\n", origin_H)
+    print ("Target_V :\n", v)
+        
+    print ("\n===Output_Matrix===")
+    print ("Matrix_W :\n", output_W)
+    print ("Matrix_H :\n", output_H)
+    print ("Output_V :\n", dot(output_W, output_H))
+    
+    
+    
+    
+if __name__ == '__main__':
+    main()
