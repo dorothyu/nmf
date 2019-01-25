02c77d51e37194eeb427b590e9798ffb76d9370e
Yangster <dorothyuyou@hotmail.com>
2019-01-25 15:18:27 +0900

    Update conses.py

--- conses.py
+++ conses.py
@@ -2,40 +2,41 @@ import nmf
 import numpy as np
 
 
-def consensus(a, Matrix_RANK, Matrix_ORDER,nloop):
+def consensus(V, rank, nloop):
     """ 
-    Calculate consensus matrix
+    Calculate consensus matrix for columns of V
+    Matrix V has the size of n rows and m columns
     """
-    (n,m)     = a.shape
-    consensus = np.zeros((m,m))
-    conn      = np.zeros((m,m))
-    connac = np.zeros((m,m))
+    shape = V.shape
+    consensus = np.zeros((shape[1],shape[1]))
+    conn = np.zeros((shape[1],shape[1]))
+    connac = np.zeros((shape[1],shape[1]))
 
     for l in range(nloop):
-        (w,h) = nmf.basicnmf(a, Matrix_RANK, 0.001, 50, 100)
-        conn   = nmfconnectivity(h)
+        (W,H) = nmf.basicnmf(V, rank, 0.001, 50, 100)
+        conn   = connectivity(H)
         connac = connac + conn
 
     consensus = connac / float(nloop)
     return consensus
 
-def nmfconnectivity(h):
+def connectivity(H):
     """ 
-    Calculate connective matrix
+    Calculate connectivity matrix
     """
-    (k,m) = h.shape
+    shape = H.shape
 
-    ar = []
-    for i in range(m):
+    l = []
+    for i in range(shape[1]):
         max_i = 0
         max_v = 0
-        for index,v in enumerate(h[:,i]):
+        for index,v in enumerate(H[:,i]):
             if v > max_v :
                 max_v = v
                 max_i = index
-        ar.append(max_i)
+        l.append(max_i)
 
-    mat1 = np.tile(np.matrix(ar),(m,1))
-    mat2 = np.tile(np.matrix(ar).T,(1,m))
+    mat1 = np.tile(np.matrix(l),(shape[1],1))
+    mat2 = np.tile(np.matrix(l).T,(1,shape[1]))
 
     return np.array(mat1 == mat2, dtype=int)
