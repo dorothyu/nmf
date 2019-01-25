6bf63261c6e304b64ba543a8e766e9faf0701db8
Yangster <dorothyuyou@hotmail.com>
2019-01-25 17:50:51 +0900

    Update nmf.py

--- nmf.py
+++ nmf.py
@@ -2,7 +2,7 @@
 # Author: Chih-Jen Lin, National Taiwan University
 # Python/numpy translation: Anthony Di Franco
 
-
+import numpy as np
 from numpy import logical_or
 from numpy import where
 from numpy import r_
@@ -23,8 +23,10 @@ def initialize(V,rank):
     m = shape[1]
     """
     shape = V.shape 
-    Winit = random.randn(shape[0], rank)# W has the size of (n x r)
-    Hinit = random.randn(rank, shape[1]) # H has the size of (r x m)
+    #Winit = random.randn(shape[0], rank)# W has the size of (n x r)
+    #Hinit = random.randn(rank, shape[1]) # H has the size of (r x m)
+    Winit = abs(random.uniform(0,1,size = (shape[0],rank)))
+    Hinit = abs(random.uniform(0,1,size = (rank,shape[1])))
     return Winit, Hinit
     
 def basicnmf(V, rank, tol, timelimit, maxiter):
@@ -55,22 +57,20 @@ def basicnmf(V, rank, tol, timelimit, maxiter):
         if projnorm<tol*initgrad or time()-initt>timelimit: 
             break
   
-    (W, gradW, iterW) = nlssubprob(V.T,H.T,W.T,tolW,1000)
-    #(W, gradW, iterW) = nlssubprob(V,W,H.T,tolW,1000)
-    
-    W = W.T
-    gradW = gradW.T
-  
-    if iterW==1: 
-        tolW = 0.1 * tolW
+        (W, gradW, iterW) = nlssubprob(V.T,H.T,W.T,tolW,1000)
+        #(W, gradW, iterW) = nlssubprob(V,W,H,tolW,1000)
+        W = W.T
+        gradW = gradW.T
+        if iterW==1: 
+            tolW = 0.1 * tolW
 
-    (H, gradH, iterH) = nlssubprob(V,W,H,tolH,1000)
+        (H, gradH, iterH) = nlssubprob(V,W,H,tolH,1000)
     
-    if iterH==1: 
-        tolH = 0.1 * tolH
+        if iterH==1: 
+            tolH = 0.1 * tolH
     
-    if iter % 10 == 0:
-        stdout.write('.')
+        if iter % 10 == 0:
+            stdout.write('.')
     print ('\nIter = %d Final proj-grad norm %f' % (iter, projnorm))
     return (W,H)
 
@@ -88,12 +88,14 @@ def nlssubprob(V, W, Hinit, tol, maxiter):
     WtV = matmul(W.T, V)
     WtW = matmul(W.T, W) 
 
-    alpha = 1; beta = 0.1;
+    alpha = 1 
+    beta = 0.1
     
     for iter in range(1, maxiter):  
         grad = matmul(WtW, H) - WtV
         projgrad = norm(grad[logical_or(grad < 0, H >0)])
-        if projgrad < tol: break
+        if projgrad < tol: 
+            break
         # search step size 
         for inner_iter in range(1,20):
             Hn = H - alpha*grad
@@ -101,19 +103,24 @@ def nlssubprob(V, W, Hinit, tol, maxiter):
             d = Hn-H
             gradd = sum(grad * d)
             dQd = sum(matmul(WtW,d) * d)
-            suff_decr = 0.99*gradd + 0.5*dQd < 0;
+            suff_decr = 0.99*gradd + 0.5*dQd < 0
             if inner_iter == 1:
-                decr_alpha = not suff_decr.all(); Hp = H;
-                if decr_alpha: 
-                    if suff_decr.all():
-                        H = Hn; break;
-                    else:
-                        alpha = alpha * beta;
+                decr_alpha = not suff_decr.all() 
+                Hp = H
+            if decr_alpha: 
+                if suff_decr.all():
+                    H = Hn 
+                    break
                 else:
-                    if not suff_decr.all() or (Hp == Hn).all():
-                        H = Hp; break;
-                    else:
-                        alpha = alpha/beta; Hp = Hn;
+                    alpha = alpha * beta
+            else:
+                if not suff_decr.all() or (Hp == Hn).all():
+                    H = Hp 
+                    break
+                else:
+                    alpha = alpha/beta
+                    Hp = Hn
+                        
     if iter == maxiter:
         print ('Max iter in nlssubprob')
     return (H, grad, iter)
